58cdce107f38d708c7acb6f14cf7d92e3c7f1e2a
ReaLzyyy <arturix00@gmail.com>
2021-08-11 16:12:13 +0300

    notes

---
+++ 4-variables.js
@@ -0,0 +1,10 @@
+var n = 10;
+if (1 <= n && n <= 100) {
+    // 1 <= n (boolean-true/or/false) && n <= 100 (boolean true/or/false)
+    //HOW OPERATORS WORK
+    // && & - operator AND (&:true/true = + ); (&:true/false = - ); (&:false/false = - ); requires 2 operands
+    // || | - opearot OR (|:true/true = + ); (|:true/false = + ); (&:false/false = - ); requires 2 operands
+    // !    - operator NOT (!:true= - ; (!:flase= + ); requires 1 operand
+    // ^    - operator XOR (extended or) (1:true/true = -) (1:)
+    console.log("between 1 and 100");
+}
