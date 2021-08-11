1179c14fead80176d8a52329a31ae6b90334fdb9
ReaLzyyy <arturix00@gmail.com>
2021-08-11 17:48:26 +0300

    more fixing

--- 4-cycles.js
+++ 4-cycles.js
@@ -2,6 +2,8 @@ var sum = 0;
 
 var i = 0;
 while (i <= 100) {
+    //cycle is a construction where there's a condition which is checked (true/false) if it's true, the body is executed
+    //while will keep executing the condition until its false, while if will execute it once
     sum += i;
     i += 2;
 }
--- 4-prime numbers2.js
+++ 4-prime numbers2.js
@@ -8,3 +8,26 @@ HUMAN STEPS:
 4. if there's remainder - prime number
 5. if there's no remainder - not a prime number
 */
+var n = 2;
+while (n <= 100) {
+    var divider = 2;
+    var prime = true;
+    while (divider < n) {
+        if (n % divider == 0) {
+            prime = false;
+        }
+        divider += 1;
+    }
+    if (prime) {
+        console.log(n);
+    }
+    n += 1;
+}
+/* 
+n: 2
+divider: 2
+prime: true
+
+print n:
+2;
+*/
