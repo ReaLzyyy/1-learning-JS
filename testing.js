ee2b432824a5421a26d8518023c7c3f2ae0ee005
ReaLzyyy <arturix00@gmail.com>
2021-08-15 16:08:52 +0300

    calling function with return global + local scope

---
+++ testing.js
@@ -0,0 +1,19 @@
+// Setup
+var sum = 0;
+
+function addThree() {
+    sum = sum + 3;
+    return sum;
+}
+
+function addFive() {
+    var sum = 5;
+    sum = sum + 5;
+    return sum
+}
+// Only change code below this line
+
+// Only change code above this line
+
+console.log(addThree());
+console.log(addFive());
